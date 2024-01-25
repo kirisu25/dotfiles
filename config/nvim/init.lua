b9750e3d77971f2bfeebd989b5cace7fc6074a7b
kirisu25 <kirisu25@gmail.com>
2024-01-25 13:32:42 +0900

    update:nvim

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,3 +1,4 @@
 require("keymap")
 require("lazyvim")
 require("basic")
+require("filetype")
---
+++ config/nvim/lua/filetype.lua
@@ -0,0 +1,45 @@
+local M = {}
+local h = require("util.helper")
+
+local function set_indent(tab_length, is_hard_tab)
+	if is_hard_tab then
+		vim.bo.expandtab = false
+	else
+		vim.bo.expandtab = true
+	end
+
+	vim.bo.shiftwidth = tab_length
+	vim.bo.softtabstop = tab_length
+	vim.bo.tabstop = tab_length
+end
+
+M.go = function()
+	set_indent(2, true)
+	h.nmap("<Leader>gt", "<cmd>bo term go test %<CR>")
+	h.nmap("<Leader>run", "<cmd>bo term go run %<CR>")
+	h.nmap("<Leader>doc", "<cmd>bo term go doc")
+end
+
+M.rust = function()
+	set_indent(4, false)
+	h.nmap("<Leader>run", "<cmd>bo term cargo run<CR>")
+	h.nmap("<Leader>ch", "<cmd>bo term cargo check<CR>")
+	h.nmap("<Leader>ct", "<cmd>bo term cargo test<CR>")
+end
+
+local ft = setmetatable(M, {
+	__index = function()
+		return function()
+			set_indent(2, true)
+		end
+	end,
+})
+
+vim.api.nvim_create_augroup("ft_augroup", {})
+vim.api.nvim_create_autocmd("Filetype", {
+	group = "ft_augroup",
+	pattern = "*",
+	callback = function(args)
+		ft[args.match]()
+	end,
+})
