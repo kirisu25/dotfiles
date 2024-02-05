79aa2e03bb232093c85e9793a20e52ecd83b562f
kirisu25 <kirisu25@gmail.com>
2024-02-05 16:28:31 +0900

    update:zsh

--- config/zsh/plugins/fzf.zsh
+++ config/zsh/plugins/fzf.zsh
@@ -18,7 +18,13 @@ source "${HOME}/.local/share/zinit/plugins/junegunn---fzf/shell/key-bindings.zsh
 
 # OPTS
 export FZF_DEFAULT_COMMAND="rg --files --hidden --glob '!.git'"
-export FZF_DEFAULT_OPTS="--height=90% --layout=reverse --info=inline --border --margin=1 --padding=1"
+export FZF_DEFAULT_OPTS="--height=90% --layout=reverse --info=inline
+--border --margin=1 --padding=1
+--color=fg:#c5cdd9,bg:#262729,hl:#6cb6eb
+--color=fg+:#c5cdd9,bg:#262729,hl:#5dbbc1
+--color=info:#88909f,prompt:#ec7279,pointer:#d38aea
+--color=marker:#a0c980,spinner:#ec7279,header:#5dbbc1
+"
 export FZF_CTRL_T_OPTS="
     --preview 'bat -n --color=always {}'
     --bind 'ctrl-/:change-preview-window(down|hidden|)'
@@ -33,8 +39,19 @@ export FZF_TMUX=1
 export FZF_TMUX_OPTS="-p 80%"
 
 # function
+if [[ $(command -v fzf) ]]; then
 ## move directory
-fd() {
-    local dir
-    dir=$(find ${1:-.} -type d 2> /dev/null | fzf --preview 'tree -C {}') && cd "$dir"
-}
+  fd() {
+      local dir
+      dir=$(find ${1:-.} -type d 2> /dev/null | fzf --preview 'tree -C {}') && cd "$dir"
+  }
+
+## file opening nvim
+  nvim_fzf() {
+    local file
+    file=$(find | fzf --preview "bat -n --color=always {}")
+    nvim $file
+  }
+  zle -N nvim_fzf
+  bindkey "^N" nvim_fzf
+fi
