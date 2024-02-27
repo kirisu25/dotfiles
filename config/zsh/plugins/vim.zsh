d69943580c337ceaf0d4edadf80c149df53be489
kirisu25 <kirisu25@gmail.com>
2024-02-27 11:29:35 +0900

    update: zsh

--- config/zsh/plugins/vim.zsh
+++ config/zsh/plugins/vim.zsh
@@ -1,7 +1,7 @@
-zinit ice as"program" atclone"rm -f src/auto/config.cache; \
-	./configure --prefix=$ZPFX --with-features=huge --enable-multibyte \
-	--enable-cscope --enable-python3interp --with-x \
-	--enable-perlinterp --enable-rubyinterp --enable-fontset \
-	--enable-xim --enable-terminal --enable-fail-if-missing" atpull"%atclone" \
-	make"install" pick"$ZPFX/bin/vim"
-zinit light vim/vim
+# zinit ice as"program" atclone"rm -f src/auto/config.cache; \
+# 	./configure --prefix=$ZPFX --with-features=huge --enable-multibyte \
+# 	--enable-cscope --enable-python3interp --with-x \
+# 	--enable-perlinterp --enable-rubyinterp --enable-fontset \
+# 	--enable-xim --enable-terminal --enable-fail-if-missing" atpull"%atclone" \
+# 	make"install" pick"$ZPFX/bin/vim"
+# zinit light vim/vim
