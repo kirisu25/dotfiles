43fa833b9b593f190264d6dad983b91ecd9449c4
kirisu25 <kirisu25@gmail.com>
2024-03-05 16:27:57 +0900

    update:nvim

--- config/nvim/lua/plugins/ipy.lua
+++ config/nvim/lua/plugins/ipy.lua
@@ -4,6 +4,9 @@ return {
   'szkny/Ipython',
   lazy = true,
   ft = {"python"},
+  dependencies = {
+    {'szkny/SplitTerm'},
+  },
   config = function()
     h.nmap("<Leader>ip", "<cmd>Ipython<CR>")
     h.vmap("<Leader>ip", "<cmd>VIpython<CR>")
