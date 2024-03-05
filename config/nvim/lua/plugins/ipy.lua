0fe362a40b7f1fd4e0ef1265867be9083ba0302f
kirisu25 <kirisu25@gmail.com>
2024-03-05 16:15:13 +0900

    update:nvim

--- config/nvim/lua/plugins/ipy.lua
+++ config/nvim/lua/plugins/ipy.lua
@@ -3,7 +3,7 @@ local h = require("util.helper")
 return {
   'szkny/Ipython',
   lazy = true,
-  ft = {"py"},
+  ft = {"python"},
   config = function()
     h.nmap("<Leader>ip", "<cmd>Ipython<CR>")
     h.vmap("<Leader>ip", "<cmd>VIpython<CR>")
