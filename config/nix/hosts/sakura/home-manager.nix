b8be7f101abdee2811792e02293e84990fcfe429
kirisu25 <kirisu25@gmail.com>
2024-03-29 14:01:49 +0900

    update:nix

--- config/nix/hosts/sakura/home-manager.nix
+++ config/nix/hosts/sakura/home-manager.nix
@@ -6,7 +6,5 @@
     ../../home-manager/cli/direnv.nix
     ../../home-manager/cli/helix.nix
     ../../home-manager/cli/develop.nix
-    ../../home-manager/cli/git.nix
-    ../../home-manager/cli/ghq.nix
   ];
 }
