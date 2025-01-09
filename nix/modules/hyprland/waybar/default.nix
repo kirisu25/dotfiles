261efdf224b97486b85bf92039d39a767e829553
kirisu25 <kirisu25@gmail.com>
2025-01-10 02:23:29 +0900

    update waybar

--- nix/modules/hyprland/waybar/default.nix
+++ nix/modules/hyprland/waybar/default.nix
@@ -1,9 +1,19 @@
 { pkgs, ... }:
+let
+  waybar-date = pkgs.writeScriptBin "waybar-date" ''
+    date +"%y/%m/%d:%a"
+  '';
+in
 {
   home.packages = with pkgs; [
     waybar
+    waybar-date
+    # (writeShellScriptBin "waybar-date" ''
+    #   date +"%y/%m/%d:%a"
+    # '')
   ];
 
   xdg.configFile."waybar/config".source = ./config;
   xdg.configFile."waybar/style.css".source = ./style.css;
+
 }
