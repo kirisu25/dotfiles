4eb7292ad743a5286dfa6605d37bdc1c0aa5a3fc
kirisu25 <kirisu25@gmail.com>
2025-02-09 14:41:49 +0900

    ref

--- nix/desktop/configuration.nix
+++ nix/desktop/configuration.nix
@@ -98,7 +98,9 @@
     enable = true;
     type = "fcitx5";
     fcitx5.addons = [ pkgs.fcitx5-mozc ];
+    fcitx5.waylandFrontend = true;
   };
+  services.xserver.desktopManager.runXdgAutostartIfNone = true;
 
   fonts.packages = with pkgs; [
     noto-fonts-cjk-serif
