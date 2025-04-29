356abeae3d0a411f95433f9939f93f97b9ac0946
kirisu25 <kirisu25@gmail.com>
2025-04-29 13:52:38 +0900

    update ghostty config

--- nix/modules/gui/ghostty/default.nix
+++ nix/modules/gui/ghostty/default.nix
@@ -3,4 +3,12 @@
   home.packages = with pkgs; [
     ghostty
   ];
+
+  
+  home.file ={
+    ".config/ghostty/config" = {
+      target = ".config/ghostty/config";
+      source = ./config;
+    };
+  };
 }
